bf470e94de4166f5954d9d33011381b9c46f345a
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-04 21:45:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,9 +3,11 @@ def introduction(name)
 puts "Hi, my name is #{name}."
 end
 
-  name = "Josh"
-  language = "Ruby"
+name = "Josh"
+language = "Ruby"
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+name = "Josh"
+language = "Ruby"
